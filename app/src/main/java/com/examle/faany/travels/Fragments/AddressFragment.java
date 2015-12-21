0dbca1ddfc3f044bc90dc8c2acb1f223ddf1fbb0
Faany <farhangaved@gmail.com>
2015-12-21 05:56:05 +0500

    code 6

--- app/src/main/java/com/examle/faany/travels/Fragments/AddressFragment.java
+++ app/src/main/java/com/examle/faany/travels/Fragments/AddressFragment.java
@@ -1,8 +1,12 @@
 package com.examle.faany.travels.Fragments;
 
+import android.app.FragmentManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +18,7 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,10 +35,14 @@ import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Filter;
 import android.widget.Filterable;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 
+import com.examle.faany.travels.COMUNICATOR;
 import com.examle.faany.travels.R;
+import com.examle.faany.travels.Utils;
+import com.google.android.gms.maps.model.LatLng;
 
 /**
  * Created by farha on 18-Dec-15.
@@ -41,15 +50,21 @@ import com.examle.faany.travels.R;
 public class AddressFragment extends Fragment implements OnItemClickListener
 {
 
+    COMUNICATOR comm;
+    Utils U;
+
+    android.support.v4.app.FragmentManager manager;
+
+    //-------------------start------for AutoComplete--------------
     private static final String LOG_TAG = "ExampleApp";
 
     private static final String PLACES_API_BASE = "https://maps.googleapis.com/maps/api/place";
     private static final String TYPE_AUTOCOMPLETE = "/autocomplete";
     private static final String OUT_JSON = "/json";
 
-    //------------ make your specific key ------------
     private static final String API_KEY = "AIzaSyAZIjFUay5Lu4KI5aG6YQ9e9xFHAiwsnt4";        //put ur own key...which has "that API"  enabled
 
+    //-------------------end------for AutoComplete--------------
 
 
 
@@ -61,15 +76,24 @@ public class AddressFragment extends Fragment implements OnItemClickListener
         return view;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
 
+        comm = new COMUNICATOR();
+        U = new Utils();
+
+        manager = getActivity().getSupportFragmentManager();
+    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState)
     {
         super.onActivityCreated(savedInstanceState);
 
-        AutoCompleteTextView autoCompView1 = (AutoCompleteTextView) getView().findViewById(R.id.autoCompleteTextView);
-        AutoCompleteTextView autoCompView2 = (AutoCompleteTextView) getView().findViewById(R.id.autoCompleteTextView1);
+        final AutoCompleteTextView autoCompView1 = (AutoCompleteTextView) getView().findViewById(R.id.autoCompleteTextView);
+        final AutoCompleteTextView autoCompView2 = (AutoCompleteTextView) getView().findViewById(R.id.autoCompleteTextView1);
 
         autoCompView1.setAdapter(new GooglePlacesAutocompleteAdapter(getActivity(), R.layout.custom_listview_autocomplete));
         autoCompView1.setOnItemClickListener(this);
@@ -77,13 +101,115 @@ public class AddressFragment extends Fragment implements OnItemClickListener
         autoCompView2.setAdapter(new GooglePlacesAutocompleteAdapter(getActivity(), R.layout.custom_listview_autocomplete));
         autoCompView2.setOnItemClickListener(this);
 
+
+        autoCompView1.setText(comm.MY_LOC);
+        autoCompView2.setHint(comm.MY_POINTER);
+        Log.d("fany", "in Address frag loc and ppointer is  " + comm.MY_LOC + "," + comm.MY_POINTER + "." );
+
+        //---------------------------------start-------------------------GEO coding---------------------
+
+
+        ImageButton calculate = (ImageButton) getActivity().findViewById(R.id.imageButton2);
+        calculate.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                String Slocation = autoCompView1.getText().toString();
+                String Elocation = autoCompView2.getText().toString();
+
+                if  (Elocation.isEmpty()  ||  Slocation.isEmpty())
+                {
+                    Toast.makeText(getActivity(), "Plzz...enter Address", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+
+
+                    Toast.makeText(getActivity(), "WAIT...(calculating)", Toast.LENGTH_LONG).show();
+
+                    Geocoder gc = new Geocoder(getActivity());     //make object
+
+                    try {
+                        //////////////////////////////
+                        List<Address> list = gc.getFromLocationName(Slocation, 1);  //"1" shows the number of string returns....becuz us naam sy related kafi places ho skti hain
+
+                        Address add = list.get(0);      //sirf phli place  hi loo
+
+                        String locality = add.getAddressLine(0);        //locality k andr  map pr jo orignal place hy  , wo andr rakhi hy list.get(0);   sy ly kr
+
+                        double lat = add.getLatitude();
+                        double lng = add.getLongitude();
+
+                        LatLng start = new LatLng(lat, lng);
+
+
+                        Log.d("Fany", "......................" + locality);
+                        ////////////////////////////////////
+                        List<Address> Elist = gc.getFromLocationName(Elocation, 1);  //"1" shows the number of string returns....becuz us naam sy related kafi places ho skti hain
+
+                        Address Eadd = Elist.get(0);      //sirf phli place  hi loo
+
+                        String Elocality = Eadd.getAddressLine(0);       //locality k andr  map pr jo orignal place hy  , wo andr rakhi hy list.get(0);   sy ly kr
+
+
+                        double Elat = Eadd.getLatitude();
+                        double Elng = Eadd.getLongitude();
+
+                        LatLng end = new LatLng(Elat, Elng);
+
+                        Log.d("Fany", "......................" + Elocality);
+                        ////////////////////////////////////
+
+                        double dist = U.CalculationByDistance(start, end);
+                        comm.DISTANCE = dist;
+
+                        comm.MY_POINTER = Elocation;
+
+                        //--------------------
+                        FragmentTransaction transaction = manager.beginTransaction();
+
+                        GeneralFragment GF = new GeneralFragment();
+                        transaction.replace(R.id.myMainLayOut, GF, "G_F");        //add General Fragment which has ADRESS & TAbs Fragments in it...
+                        //transaction.addToBackStack(null);
+                        transaction.commit();
+
+                        ///--------------------
+
+
+
+                        ///-----
+                        Log.d("Fany", "_________________________________________ distance is " + dist);
+
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+
+                }
+
+            }
+        });
+
+        //---------------------------------end-------------------------GEO coding---------------------
+
     }
+
+
+
+
+
+
+
 
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long id)
     {
         String str = (String) adapterView.getItemAtPosition(position);
         Toast.makeText(getActivity(), str, Toast.LENGTH_SHORT).show();
     }
+
 
     public static ArrayList<String> autocomplete(String input)
     {
@@ -140,6 +266,8 @@ public class AddressFragment extends Fragment implements OnItemClickListener
 
         return resultList;
     }
+
+
 
     class GooglePlacesAutocompleteAdapter extends ArrayAdapter<String> implements Filterable
     {
--- app/src/main/java/com/examle/faany/travels/Fragments/MapFragment.java
+++ app/src/main/java/com/examle/faany/travels/Fragments/MapFragment.java
@@ -1,15 +1,20 @@
 package com.examle.faany.travels.Fragments;
 
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 
+import com.examle.faany.travels.COMUNICATOR;
 import com.examle.faany.travels.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,14 +25,25 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 /**
  * A fragment that launches other parts of the demo application.
  */
 public class MapFragment extends Fragment
 {
 
+    COMUNICATOR comm;
+
+    //-----for map fragment-----
     MapView mMapView;
     private GoogleMap googleMap;
+
+
+    //----for reverse geo coder----
+    String address = "";
+    String city = "";
 
     FragmentManager manager;
 
@@ -38,6 +54,8 @@ public class MapFragment extends Fragment
 
         manager = getFragmentManager();
 
+        comm = new COMUNICATOR();
+
     }
 
     @Override
@@ -45,7 +63,7 @@ public class MapFragment extends Fragment
     {
         View v = inflater.inflate(R.layout.fragment_map, container, false);
 
-        //-------------------------rata CODE start----------------------
+        //-------------------------MAP rata CODE start----------------------
         mMapView = (MapView) v.findViewById(R.id.mapView);      //ye us Map ki Id hy , jo hum ny fragment_map layout may banaya hy...
         mMapView.onCreate(savedInstanceState);
         mMapView.onResume();// needed to get the map to display immediately
@@ -58,11 +76,12 @@ public class MapFragment extends Fragment
             e.printStackTrace();
         }
         googleMap = mMapView.getMap();
-        //-------------------------rata CODE end----------------------
+        //-------------------------MAP rata CODE end----------------------
 
 
+        //------------------------------MAP cutomization start----------------------------
         LatLng Pak = new LatLng(33.6667, 73.1667);       //coordinates of pakistan...by default it is shown...pakistan
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Pak, 3));      //start may camera idhr jaaye....level of zoom...in increasing order...
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Pak, 4));      //start may camera idhr jaaye....level of zoom...in increasing order...
 
         googleMap.setMyLocationEnabled(true);     //to enable zoom button to get my location
 
@@ -70,10 +89,51 @@ public class MapFragment extends Fragment
         //map.addMarker(new MarkerOptions().icon(BitmapDescriptorFactory.fromResource(R.drawable.lolo)).anchor(0.0f, 1.0f).position(sydney)); // Anchors the marker on the bottom left
 
         googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);      //map ki type....
+        //------------------------------MAP cutomization end----------------------------
 
-        //copy paste here...
 
-        //----------------------------------------------------------
+        //------start---------to get latitiude and longitude of curent location-------------------------------------
+
+        GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener()
+        {
+
+            @Override
+            public void onMyLocationChange (Location location)
+            {
+                LatLng loc = new LatLng (location.getLatitude(), location.getLongitude());
+
+                //---------------now , convert that lat and lang to address---------by using inverse geoCoder.....
+
+                Geocoder geoCoder = new Geocoder(getActivity());
+                List<Address> matches = null;
+                try
+                {
+                    matches = geoCoder.getFromLocation(loc.latitude, loc.longitude, 1);
+                }catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+                Address bestMatch = (matches.isEmpty() ? null : matches.get(0));
+                //Address has the complete adress , now convert it into strings
+
+                address = bestMatch.getAddressLine(0);
+                city = bestMatch.getLocality();
+                //String state = bestMatch.getAdminArea();
+                //String country = bestMatch.getCountryName();
+                //String knownName = bestMatch.getFeatureName();
+                Log.d("fany", loc.latitude + " , " + loc.longitude + " , " + address + " , " + city);
+
+
+                String completeAddress = address + " " + city;
+                comm.MY_LOC = completeAddress;
+
+            }
+        };
+        googleMap.setOnMyLocationChangeListener(myLocationChangeListener);
+
+        //-------end--------to get latitiude and longitude of curent location-------------------------------------
+
+
 
         return v;
     }
@@ -100,7 +160,7 @@ public class MapFragment extends Fragment
 
                                       GeneralFragment GF = new GeneralFragment();
                                       transaction.add(R.id.myMainLayOut, GF, "G_F");        //add General Fragment which has ADRESS & TAbs Fragments in it...
-                                      transaction.addToBackStack(null);
+                                      //transaction.addToBackStack(null);
                                       transaction.commit();
 
 
